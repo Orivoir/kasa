eae872787f4240b6790baef9955cf6e69bc6a1a7
orivoir <sam.gabor@hotmail.com>
2024-11-08 18:47:10 +0100

    Add content of About page

--- src/Accordion/atomes/Content.jsx
+++ src/Accordion/atomes/Content.jsx
@@ -4,43 +4,8 @@ import styles from "./Content.module.scss"
 
 export default function Content({isOpen, children}) {
 
-  const ref = React.useRef(null)
-
-  const onFetchRef = el => {
-    if(!el) return;
-
-    ref.current = el
-    if(!isOpen) {
-      ref.current.classList.add("hide")
-    }
-  }
-
-  const onFinishTransition = event => {
-
-    if(event.propertyName !== "opacity") return;
-
-    if(event.target === ref.current) {
-
-      const currentState = getComputedStyle(ref.current).opacity
-
-      if(currentState == 0) {
-        ref.current.classList.add("hide")
-      } else {
-        ref.current.classList.remove("hide")
-      }
-    }
-  }
-
-  React.useEffect(() => {
-    addEventListener("transitionend", onFinishTransition)
-
-    return () => {
-      addEventListener("transitionend", onFinishTransition)
-    }
-  }, [])
-
   return (
-    <div ref={onFetchRef} className={`${styles.AccordionContent} ${isOpen ? styles.Open: ""}`}>
+    <div className={`${styles.AccordionContent} ${isOpen ? styles.Open: ""}`}>
       {children}
     </div>
   )
